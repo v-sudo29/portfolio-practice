46a18c4e218305fbd55af450ce8e0d216e4003f4
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-05-11 09:53:48 -0700

    Change image unit to rem Create About Me skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Heading from "./components/Hero";
+import Heading from "./components/Heading";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Heading />
       <div className='triangle'></div>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <main className='main'>
+      <section className='about-me'>
+        <h2> About Me </h2>
+      </section>
+    </main>
+  )
+}

7d28caa98519ef645616ac5080f637c7173a2c86
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-05-12 13:36:09 -0700

    Implement minimum 1000px responsiveness

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,7 @@
 import Socials from "./Socials";
 
 export default function AboutMe() {
-  const dummyText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis, magnam ex voluptatibus dolor veliste sunt doloremque labore fugit obcaecati iure est recusandae error nisi provident, magni culpaeaque!. Saepe fugit totam necessitatibus enim placeat impedit quas debitis vitae. Deleniti animi magnameos nisi dolores dicta, mollitia quidem odit non amet. Deleniti animi magnameos nisi dolores dicta, mollitia quidem odit non amet.';
+  const dummyText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis, magnam ex voluptatibus dolor veliste sunt doloremque labore fugit obcaecati iure est recusandae error nisi provident, magni culpaeaque!. Saepe fugit totam necessitatibus enim placeat impedit quas debitis vitae. Deleniti animi magnameos nisi dolores dicta, mollitia quidem odit non amet. Deleniti animi magnameos nisi dolores dicta, mollitia quidem odit non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis, magnam ex voluptatibus dolor veliste sunt doloremque labore fugit obcaecati iure est recusandae error nisi provident, magni culpaeaque!. Saepe fugit totam necessitatibus enim placeat impedit quas debitis vitae. Deleniti animi magnameos nisi dolores dicta, mollitia quidem odit non amet. Deleniti animi magnameos nisi dolores dicta, mollitia quidem odit non amet.';
 
   return (
     <section className='about-me'>
